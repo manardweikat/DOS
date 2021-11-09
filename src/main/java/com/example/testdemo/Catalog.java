521f318ba8df94320c354e647b50b1ba4204a944
Dell <manar>
2021-11-09 18:09:04 +0200

    search

--- src/main/java/com/example/testdemo/Catalog.java
+++ src/main/java/com/example/testdemo/Catalog.java
@@ -2,6 +2,7 @@ package com.example.testdemo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -12,19 +13,33 @@ import java.util.List;
 
 @Controller
 public class Catalog {
-    List<Book> books=TestdemoApplication.books;
 
-@GetMapping()
-public  @ResponseBody String r(){
-    String d="manar";
-    d=d.replace("a","x");
-    return d;
+
+@GetMapping("/search")
+public  @ResponseBody String search(@RequestParam String topic) throws IOException {
+    File file =new File("book.txt");
+    BufferedReader bufferedReader=new BufferedReader(new FileReader(file));
+    StringBuffer stringBuffer=new StringBuffer();
+    stringBuffer.append("{\"Topic\":\""+topic+"\"");
+    String book; List<String> book1=new ArrayList<>();String oldcost; String Content="";
+    while ((book=bufferedReader.readLine())!=null){
+        book1=Arrays.asList(book.split(","));
+        if(book1.get(2).equals(topic)){
+            stringBuffer.append("{\"id\":\""+book1.get(0)+",\"");
+            stringBuffer.append("\"title\":\""+book1.get(1)+"\"},");
+//            stringBuffer.append("\n");
+
+
+        }
+
+    }
+    return stringBuffer.toString();
 }
 
     @GetMapping("/book1")
     public @ResponseBody String name(@RequestParam String id,@RequestParam String cost) throws IOException {
-        List<String>B=new ArrayList<>();
-        File file =new File("books.txt");
+
+        File file =new File("book.txt");
         BufferedReader bufferedReader=new BufferedReader(new FileReader(file));
         StringBuffer stringBuffer=new StringBuffer();
         String book; List<String> book1=new ArrayList<>();String oldcost; String Content="";
