64b1036a577e514720f3a5dabbb49ed91916e641
manardweikat <60611836+manardweikat@users.noreply.github.com>
2021-11-10 19:34:47 +0200

    Add files via upload

---
+++ order/src/main/java/com/example/testdemo/Order.java
@@ -0,0 +1,47 @@
+package com.example.testdemo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class Order {
+
+
+    @PutMapping("/purchase")
+    public @ResponseBody String name(@RequestParam int id) throws IOException {
+
+        File file =new File("C:/Spring-Temp/DOS/book.txt");
+        BufferedReader bufferedReader=new BufferedReader(new FileReader(file));
+        StringBuffer stringBuffer=new StringBuffer();
+        String book; List<String> book1=new ArrayList<>();String oldnum; String new_n="";
+        String res="purchase request fail";
+        while ((book=bufferedReader.readLine())!=null){
+            book1= Arrays.asList(book.split(","));
+            if(book1.get(0).equals(String.valueOf(id))) {
+                if (Integer.parseInt(book1.get(3)) > 0) {
+                    oldnum = book1.get(3);
+                    int newnum = Integer.parseInt(oldnum) - 1;
+                    new_n = String.valueOf(newnum);
+                    book = book.replace(oldnum, new_n);
+                    res = "purchase request done";
+                }
+            }
+            stringBuffer.append(book);
+            stringBuffer.append("\n");
+        }
+        FileWriter fileWriter=new FileWriter(file);
+        fileWriter.write(stringBuffer.toString());
+        bufferedReader.close();
+        fileWriter.close();
+
+        return res;
+    }
+}
---
+++ order/src/main/java/com/example/testdemo/OrderApplication.java
@@ -0,0 +1,17 @@
+package com.example.testdemo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootApplication
+public class OrderApplication {
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApplication.class, args);
+	}
+
+}
